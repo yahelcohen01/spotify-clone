072a15a83ea0521157609fe6d0d74f839d2f0055
yahelcohen01 <yahelcohen01@gmail.com>
2024-07-27 18:48:35 +0300

    add > sorting in library with no functionality

--- src/components/global/Sidebar/Library/SortButton.tsx
+++ src/components/global/Sidebar/Library/SortButton.tsx
@@ -1,21 +1,125 @@
-import { Button, styled } from "@mui/material";
+import {
+  Button,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  styled,
+  Typography,
+} from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import CheckIcon from "@mui/icons-material/Check";
 
-export const SortButton = styled(Button)({
-  boxShadow: "none",
-  fontWeight: 500,
-  color: "#b3b3b3",
-  textTransform: "none",
-  fontSize: ".6em",
-  padding: "6px 12px",
-  lineHeight: 1.5,
-  transition: "all 0.3s ease",
-  backgroundColor: "transparent",
-  borderColor: "transparent",
-  "&:hover": {
-    backgroundColor: "transparent",
-    borderColor: "transparent",
-    boxShadow: "none",
-    color: "#fff",
-    transform: "scale(1.1)",
-  },
-});
+const sortingOptions = [
+  "Recents",
+  "Recentley Added",
+  "Alphabetical",
+  "Creator",
+];
+
+export const SortButton = () => {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [selectedIndex, setSelectedIndex] = useState(1);
+  const open = Boolean(anchorEl);
+
+  const openPopover = (e: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const closePopover = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuItemClick = (
+    event: React.MouseEvent<HTMLElement>,
+    index: number
+  ) => {
+    setSelectedIndex(index);
+  };
+
+  return (
+    <>
+      <Button
+        sx={{
+          boxShadow: "none",
+          fontWeight: 500,
+          color: "#b3b3b3",
+          textTransform: "none",
+          fontSize: ".6em",
+          padding: "6px 12px",
+          lineHeight: 1.5,
+          transition: "all 0.3s ease",
+          backgroundColor: "transparent",
+          borderColor: "transparent",
+          "&:hover": {
+            backgroundColor: "transparent",
+            borderColor: "transparent",
+            boxShadow: "none",
+            color: "#fff",
+            transform: "scale(1.1)",
+          },
+        }}
+        disableRipple
+        variant="contained"
+        onClick={openPopover}
+      >
+        {sortingOptions[selectedIndex]}
+      </Button>
+      <Menu
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        onClose={closePopover}
+        MenuListProps={{
+          "aria-labelledby": "lock-button",
+          role: "listbox",
+          sx: {
+            paddingRight: "4px",
+            paddingLeft: "4px",
+          },
+        }}
+        // sx={{ paddingRight: "4px", paddingLeft: "4px" }}
+      >
+        <Typography
+          className="px-4 py-2 opacity-60 text-xs"
+          fontWeight={500}
+          fontSize={"0.75rem"}
+        >
+          Sort by
+        </Typography>
+        {sortingOptions.map((option, index) => {
+          const isSelected = index === selectedIndex;
+          return (
+            <MenuItem
+              key={option}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                color: isSelected ? "#1fcd5d" : "#e5e5e5",
+                minWidth: "190px",
+                borderRadius: "2px",
+              }}
+              selected={isSelected}
+              onClick={(event) => handleMenuItemClick(event, index)}
+              disableRipple
+            >
+              <Typography
+                variant="subtitle2"
+                color={isSelected ? "#1fcd5d" : "#e5e5e5"}
+                fontWeight={400}
+              >
+                {option}
+              </Typography>
+              {isSelected && <CheckIcon fontSize="small" />}
+            </MenuItem>
+          );
+        })}
+      </Menu>
+    </>
+  );
+};
